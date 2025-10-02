7bd6ad1b068bf61a0740ba385468b39e0ffaba69
MD. Shohel Rana <shohel065931@gmail.com>
2025-10-02 13:48:54 +0600

    vv

--- index.php
+++ index.php
@@ -14,8 +14,16 @@
         </div>
         <div class="search">
            <form method="">
-            <input type="search" name="search" id="" placeholder="Search Products">
+            <input type="search" name="search" id="" placeholder="Search Products" required>
+            <input type="submit" value="Search">
            </form>
+        </div>
+        <div class="cart_icon">
+            <ul>
+                <li><a href="#">Favorite</a></li>
+                <li><a href="#">Cart</a></li>
+                <li><a href="#">Account</a></li>
+            </ul>
         </div>
     </div>
 </header>
